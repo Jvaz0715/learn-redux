e1d290013a159dff44bd971f76cfed6c940ae7b9
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-11-18 10:25:41 -0500

    ...

--- src/redux/index.js
+++ src/redux/index.js
@@ -4,12 +4,8 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 
 import rootReducer from '../reducers';
 
-const initialState = {};
-const middleware = [thunk];
-
 const store = createStore(
-   rootReducer,
-   initialState, 
+   rootReducer, 
    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
