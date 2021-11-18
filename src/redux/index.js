b269502f25dec38d82cd32beaa4ec1e579f03ac2
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-11-18 10:26:17 -0500

    fixes store

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,4 @@
-import {createStore, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import {createStore} from 'redux'
 
 import rootReducer from '../reducers';
 
